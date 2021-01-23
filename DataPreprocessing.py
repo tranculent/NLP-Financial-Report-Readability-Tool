2b1127bc6d4bc653b3d39f7cf2a68b0572c4ae24
Yanko <yanicha93@gmail.com>
2021-01-23 11:35:05 +0200

    added support for textual data extraction instead of the numerical features in .csv file.

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -17,41 +17,55 @@ from itertools import islice
 """***Data Preprocessing tools***"""
 
 class CSVManager:
-    def __init__(self, csv_name):
+    def __init__(self, csv_name, directory):
         print("Loading csv file...")
         self.dataset = pd.read_csv(csv_name)
-        self.header = np.concatenate((self.dataset.head(0).columns[:11], self.dataset.head(0).columns[13:]))
-
-        self.chosen_reports = {}
-        self.X, self.y = self.extract_columns()
-        
-        self.X, self.y = self.reshape_columns(self.X, self.y)
-        self.y = self.y[:23018]
-        
+        self.directory = directory
+        # self.header = np.concatenate((self.dataset.head(0).columns[:11], self.dataset.head(0).columns[13:]))
+        self.corpus, self.y = self.extract_columns()
+        self.y = self.reshape_columns(self.y)
         
     def extract_columns(self):
+        print("     Extracting columns...")
         self.dataset = self.dataset.dropna(how='all')
-        X = []
         y = []
-
+        corpus = []
+        c = 0
         for i in range(2, len(self.dataset.iloc[:,:])):
-            temp = []
+            count = 0
+            # Check for eligible rows
             for j in range(len(self.dataset.iloc[i,:])):
-                if j != 11 and j != 12 and j != 1 and j != 2 and j != 3 and j != 4 and j != 5 and j != 13 and j != 9 and j != 36 and j != 37:
-                    if type(self.dataset.iloc[i,j]) is not str:
-                        if not np.isnan(self.dataset.iloc[i,j]):
-                            temp.append(int(self.dataset.iloc[i,j]))
-            if len(temp) > 14:
-                # if temp[14] == 1:
-                if len(X) > len(y) + 1:
-                    break
-                else:
-                    X.append(temp)
-                y.append(self.dataset.iloc[i,12])
-                self.chosen_reports[self.dataset.iloc[i, 1][:-4]] = self.dataset.iloc[i, 2]
-                    #print(self.dataset.iloc[i, 2])
+                if self.dataset.iloc[i,14] == 1 or self.dataset.iloc[i,16] == 1:
+                    if j != 11 and j != 12 and j != 1 and j != 2 and j != 3 and j != 4 and j != 5 and j != 13 and j != 9 and j != 36 and j != 37:
+                        if type(self.dataset.iloc[i,j]) is not str and not np.isnan(self.dataset.iloc[i,j]): #front1 = 14, rear1 = 15, front2 = 16, rear2 = 17
+                            count += 1
             
-        return (X, y)
+            if count > 14:
+                folder = self.dataset.iloc[i,1][:-4]
+                if folder[-1] == " ":
+                    folder = folder[:-1]
+
+                if len(os.listdir(self.directory + "/" + folder)) != 0:
+                    for text_file in os.listdir(self.directory + "/" + folder + "/" + "sections_text"):
+                        t = ""
+                        if text_file[1] == "_":
+                            t = text_file[2:]
+                        elif text_file[2] == "_":
+                            t = text_file[3:]
+                                                        
+                        if t[:-4] == self.dataset.iloc[i,2]:
+                            f = open(self.directory + "/" + folder + "/" + "sections_text" + "/" + text_file)
+                            contents = re.sub('[^a-zA-Z]', ' ', f.read()).lower().split()
+                            # ps = PorterStemmer()
+                            # contents = [ps.stem(word) for word in contents if word not in stopwords.words('english')]
+                            contents = [word for word in contents if word not in stopwords.words('english')]
+                            corpus.append(' '.join(contents))
+                            y.append(self.dataset.iloc[i,12])
+                            f = f.close()
+                            c += 1
+                        
+        print("Count: " + str(c))
+        return (corpus, y)
     
     def is_valid(self, i):
         for j in range(len(self.dataset.iloc[i, :])):
@@ -60,33 +74,25 @@ class CSVManager:
                     return False
         return True
 
-    def reshape_columns(self, X, y):
-        X = np.reshape(X, (23717,27))
-        y = np.reshape(y, (23717, 1))
-        return (X, y)
-
-    def get_X_length(self):
-        return len(self.X)
+    def reshape_columns(self, y):
+        y = np.reshape(y, (len(y), 1))
+        return y
 
     def get_y_length(self):
         return len(self.y)
-
-    def get_X(self):
-        return self.X
     
     def get_y(self):
         return self.y
 
-    def get_chosen_reports(self):
-        return self.chosen_reports
+    def get_corpus(self):
+        return self.corpus
 
 class FileExtraction:
-    def __init__(self, directory, chosen_reports):
+    def __init__(self, directory):
         self.directory = directory
         self.corpus = {}
     
-        self.extract_sections(chosen_reports)
-        
+        self.extract_sections()
         #self.bag_of_words = {}
         #self.extract_words_counts()
         self.print_corpus_info()
@@ -109,22 +115,18 @@ class FileExtraction:
         # print("First two keys: " + str(dict(islice(self.corpus.items(), 2))))
         # print("Number of items: " + str(len(self.corpus.items())))
     
-    def extract_sections(self, chosen_reports):
+    def extract_sections(self):
         print("Extracting sections from pdfs...")
         count = 0
         for folder in os.listdir(self.directory):
             if not os.path.isfile(folder):
-                if count == 23017:
-                    print("Last corpus element: " + list(self.corpus.keys())[-1])
                 if len(os.listdir(self.directory + "/" + str(folder))) != 0:
                     self.corpus[folder] = []
                     for text_file in os.listdir(self.directory + "/" + str(folder) + "/" + "sections_text"):
                         f = open(self.directory + "/" + folder + "/" + "sections_text" + "/" + text_file)
                         contents = re.sub('[^a-zA-Z]', ' ', f.read()).lower().split()
-                        # ps = PorterStemmer()
-                        all_stopwords = stopwords.words('english')
-                        all_stopwords.remove('not')
-                        contents = [word for word in contents if not word in set(all_stopwords)]
+                        ps = PorterStemmer()
+                        contents = [ps.stem(word) for word in contents if word not in stopwords.words('english')]
                         temp_dict = {}
                         temp_dict[text_file] = ' '.join(contents)
                         self.corpus[folder].append(temp_dict)
