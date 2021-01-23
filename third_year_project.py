fda116210ec524f82b41631e343036f235569e64
Yanko <yanicha93@gmail.com>
2021-01-23 11:38:11 +0200

    Updated to be working with new code

--- third_year_project.py
+++ third_year_project.py
@@ -12,32 +12,27 @@ from ModelTrain import Training
 
 import os
 
+import numpy as np
+
 # change directory
 os.chdir(r"D:\Third Year Project\CFIE-FRSE-master\output")
 
 directory = os.getcwd()
 
-csvManager = CSVManager("output.csv")
+csvManager = CSVManager("output.csv", directory)
 
-dp = FileExtraction(directory, csvManager.get_chosen_reports())
+# dp = FileExtraction(directory)
 
-training = Training(dp.get_corpus(), csvManager.get_y())
-
-'''
-TODO:
-    - Only extract texts into corpus that 14th column is equal to 1
-        - do it by having an array of IDs extracted from the CSVManager
-        - each time a text is added to corpus, increment the ID
-        - when the temp_id has reached 3155 (the number of ids), stop
-    - that way, there are gonna be 3155 reports (which will be converted to numerical)
-        and 3155 y's => therefore able to be put into training model
-'''
+training = Training(csvManager.get_corpus(), csvManager.get_y())
 
 '''
 train the model on the corpus, 
 and then evaluate with combination of 
 the numerical features (make a formula)
 '''
+
+# 10000 0.010 random forest
+# 10000 -0.138 decision tree
 
 # TODO:
     # Add bag of words
